317a258f78dca8efc6b081715706ddc18bd598df
Gerald Cells <37741184+geraldcells18@users.noreply.github.com>
2020-02-23 11:38:01 +0800

    Some Changes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { MainControllerService } from '../services/main-controller.service';
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: './tab3.page.html',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab3Page implements OnInit {
 
-  constructor() { }
+  myCartList: any;
+  cartCount: number;
+  prodList: any;
+
+  constructor(
+    private mainController: MainControllerService,
+  ) {
+    this.myCartList = [];
+    this.prodList = [];
+  }
+
+
+  private getProducts() {
+    this.mainController.getProductList().toPromise().then(r => {
+      this.prodList = JSON.parse(r);
+      this.prodList.reverse();
+      this.prodList.map(v => {
+        v.ProductPrice = 'Buko';
+      });
+    });
+  }
 
   ngOnInit() {
+    this.getProducts();
   }
 
 }
